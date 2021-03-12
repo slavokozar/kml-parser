ce1ccc3210ab86b98c62b98fc4a85b7e0cb9ba6e
Slavo Kozar <slavo.kozar@gmail.com>
2021-03-12 13:34:49 +0100

    kml parser'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
+import { kml } from "@tmcw/togeojson";
+
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [file, setFile] = useState( null );
+    const [status, setStatus] = useState('No file selected');
+
+    const readFile = () => {
+        if (!file) {
+            setStatus('No file selected');
+            return;
+        }
+
+        setStatus('Loading File');
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            setStatus('File Loaded');
+            const content = e.target.result;
+
+
+            setStatus('XML parsing');
+            const xml = convertToXmlDom(content);
+
+            if(!xml){
+                setStatus('XML parsing failed')
+                return;
+            }
+
+            setStatus('Converting to GeoJSON');
+
+            const geoJson = kml(xml);
+            setStatus('Success');
+            console.log(geoJson);
+        };
+
+        reader.readAsText(file);
+    }
+
+    const convertToXmlDom = (text) => {
+        const parser = new DOMParser();
+
+        const xml = parser.parseFromString(text,"text/xml");
+
+        if(xml.getElementsByTagName("parsererror").length > 0){
+            return null;
+        }
+        return xml;
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div style={{width: '75vw',display: 'flex', justifyContent:'space-evenly'}}>
+                    <input
+                        type="file"
+                        onChange={( e ) => {
+                            setFile( e.target.files[ 0 ] )
+                        }}
+                    />
+                    <button onClick={() => readFile()}>
+                        Convert!
+                    </button>
+                </div>
+                <h2>{status}</h2>
+
+            </header>
+        </div>
+    );
 }
 
 export default App;
